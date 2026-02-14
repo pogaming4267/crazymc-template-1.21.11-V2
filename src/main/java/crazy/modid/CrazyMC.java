215cd6f9e0e7dece4244defdafabb1d325bd58a8
pogaming4267 <pogaming921@gmail.com>
2026-02-14 17:04:00 +0200

    The item does not apear in the creative COMBAT tab.

--- src/main/java/crazy/modid/CrazyMC.java
+++ src/main/java/crazy/modid/CrazyMC.java
@@ -1,7 +1,10 @@
 package crazy.modid;
 
+import crazy.modid.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +12,10 @@ public class CrazyMC implements ModInitializer {
 	public static final String MOD_ID = "crazymc";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	public static void initialize(){
+			ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT)
+				.register((itemGroup)-> itemGroup.accept(ModItems.BOB));
+	}
 	@Override
 	public void onInitialize() {
 
