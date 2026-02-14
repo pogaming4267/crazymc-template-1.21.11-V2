90496f2474b1e9138ce54bef1f5adc833c98214e
pogaming4267 <pogaming921@gmail.com>
2026-02-14 16:27:16 +0200

    Initial commit

---
+++ src/main/java/crazy/modid/CrazyMC.java
@@ -0,0 +1,18 @@
+package crazy.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CrazyMC implements ModInitializer {
+	public static final String MOD_ID = "crazymc";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
